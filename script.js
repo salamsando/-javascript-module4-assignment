b9e7a28ff651f6245886b5653ef2e579ed6c3162
Yaslam Salman Sando <123530420+salamsando@users.noreply.github.com>
2025-12-24 18:56:50 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+let foods = ["waakye", "beans", "jollof", "ampesi", "banku"];
+
+
+console.log("First food:", foods[0]);
+console.log("Last food:", foods[foods.length - 1]);
+
+
+foods.push("konkonte");
+console.log("Updated foods array:", foods);
+
+foods.forEach(function(food) {
+  console.log(food);
+});
+
+
+
+
+
+let student = {
+  name: "Yaslam",
+  age: 20,
+  course: "Computer Engineering"
+};
+
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
+
+
+let students = [
+  { name: "Yaslam", age: 20, course: "Computer Engineering" }
+  { name: "Stephen", age: 19, course: "Statistics" },
+  { name: "Yaw", age: 22, course: "Food Science" }
+];
+
+students.forEach(function(student) {
+  console.log(student.name);
+});
+ 
+
+
+
+
+let studentWithMethod = {
+  name: "Yaslam",
+  age: 20,
+  course: "Computer Engineering",
+  greet: function() {
+    console.log(`Hello, my name is ${this.name} and I am learning JavaScript.`);
+  }
+};
+
+studentWithMethod.greet();
+
+
